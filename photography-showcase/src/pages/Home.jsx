1da201c30beb938bc4be75ea32ed152e48023c41
julia <juliaedyck@gmail.com>
2025-08-11 14:44:47 +0200

    02_02_b

--- photography-showcase/src/pages/Home.jsx
+++ photography-showcase/src/pages/Home.jsx
@@ -13,16 +13,7 @@ function Home() {
   return (
     <>
 
-      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {photos.map((photo) => (
-          <PhotoCard
-            key={photo.id}
-            imageUrl={photo.image}
-            title={photo.title}
-            category={photo.category}
-          />
-        ))}
-      </main>
+      
     </>
   );
 }
