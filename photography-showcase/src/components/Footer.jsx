9530abd1f37dc36fa011b846109be312dfdb032f
julia <juliaedyck@gmail.com>
2025-08-11 14:52:20 +0200

    02_01_b

--- photography-showcase/src/components/Footer.jsx
+++ photography-showcase/src/components/Footer.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-gray-100 text-gray-600 p-4 text-center text-sm">
-    <span>&copy; {new Date().getFullYear()} Photography Showcase. All rights reserved.</span>
-  </footer>
+
 );
 
 export default Footer; 
