2f37b71077442370c8ca850d1dee875de188ba80
juliaedyck <juliaedyck@gmail.com>
2025-09-28 15:07:36 +0200

    final

--- photography-showcase/src/pages/Home.jsx
+++ photography-showcase/src/pages/Home.jsx
@@ -93,7 +93,7 @@ function Home() {
       </div>
      
       <main className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5 gap-4 sm:gap-6 lg:gap-8 items-start justify-start">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4 sm:gap-6 lg:gap-8 items-start justify-start">
           {filteredPhotos.map((photo, index) => (
             <MemoizedPhotoCard key={photo.id || photo.url} photo={photo} index={index} />
           ))}
